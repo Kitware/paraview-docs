157eb4b3a371d481a2f82797375b6af6d452df2c
buildbot <buildbot@kwrobot02>
2018-09-26 16:07:52 -0400

    update docs for '5.6.0-RC2-145-g54da5732c8' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-RC2-136-gb6a1bcffd5',
+    VERSION: '5.6.0-RC2-145-g54da5732c8',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
