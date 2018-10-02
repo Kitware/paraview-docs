357c15cffd5d6a2e88668f5fa3e0d4f034c76d53
buildbot <buildbot@kwrobot02>
2018-10-01 20:55:50 -0400

    update docs for '5.6.0-RC2-42-gfc1fee96d0' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-RC2-40-ga2f7d00271',
+    VERSION: '5.6.0-RC2-42-gfc1fee96d0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
