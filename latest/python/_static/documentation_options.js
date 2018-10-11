697be7f66e2211f362981ad9e9736c844986fc5c
buildbot <buildbot@kwrobot02>
2018-10-11 16:16:12 -0400

    update docs for '5.6.0-RC2-79-g9ee22766ed' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-RC2-70-geed584b1e7',
+    VERSION: '5.6.0-RC2-79-g9ee22766ed',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
