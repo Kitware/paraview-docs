604dc27f3bb7e31a5d739898ed42434f279d7e9b
buildbot <buildbot@kwrobot02>
2018-11-06 17:26:17 -0500

    update docs for '5.6.0' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-RC2-91-g7e8c93bc2f',
+    VERSION: '5.6.0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
