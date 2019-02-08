2602c348c62620d29757a97b08af140de079c824
buildbot <buildbot@kwrobot02>
2019-02-08 02:27:24 -0500

    update docs for '5.6.0-17-g77e35a3287' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-13-g04d2b0c4aa',
+    VERSION: '5.6.0-17-g77e35a3287',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
