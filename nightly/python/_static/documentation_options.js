9c6cde9a7bea08cf0d7c6b69380c54e10db2397f
buildbot <buildbot@kwrobot02>
2019-05-17 11:24:56 -0400

    update docs for '5.6.0-1577-gcd0ea8b2d0' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.6.0-1571-g94c3061b9a',
+    VERSION: '5.6.0-1577-gcd0ea8b2d0',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
