363e96ddba2e4f89af2706b144f52b62fb8e5172
buildbot <buildbot@kwrobot02>
2019-07-12 15:57:30 -0400

    update docs for '5.7.0-RC1-57-g8c0cd806d6' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.0-RC1-55-g778f83344d',
+    VERSION: '5.7.0-RC1-57-g8c0cd806d6',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
