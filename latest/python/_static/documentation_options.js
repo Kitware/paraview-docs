f0719de126feae787e11c900a5158b328880edd8
buildbot <buildbot@kwrobot02>
2019-07-16 22:35:45 -0400

    update docs for '5.7.0-RC1-27-g6e143d1447' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.0-RC1-25-gaa570cdbed',
+    VERSION: '5.7.0-RC1-27-g6e143d1447',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
