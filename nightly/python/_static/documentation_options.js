6ecb60dbfeb050cd3331858019616e089bdc983d
buildbot <buildbot@kwrobot02>
2019-07-23 09:13:41 -0400

    update docs for '5.7.0-RC1-112-gb12d807f55' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.0-RC1-105-gb750399b70',
+    VERSION: '5.7.0-RC1-112-gb12d807f55',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
