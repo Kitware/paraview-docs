cbb16200053d88d79eede7e510180ab84bc9863b
buildbot <buildbot@kwrobot02>
2019-08-15 23:22:33 -0400

    update docs for '5.7.0-RC2-28-g60e07559c2' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.0-RC2-26-g7e8122e66b',
+    VERSION: '5.7.0-RC2-28-g60e07559c2',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
