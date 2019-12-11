c6a4edad04cf5de71153e7c3daa0aeb880e5b0f5
buildbot <buildbot@kwrobot02>
2019-12-11 13:04:32 -0500

    Update documentation for version nightly

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.7.0-873-gff6b66ab5c',
+    VERSION: '5.7.0-874-g887fd42ac5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
