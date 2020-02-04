85a0c46f536a7fc5a820dcd9fcf80e30303dd160
buildbot <buildbot@kwrobot02>
2020-02-03 21:32:49 -0500

    update docs for '5.8.0-RC2-15-gc5383ba989' as 'latest'

--- latest/python/_static/documentation_options.js
+++ latest/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-RC2-13-g29f0e7dc47',
+    VERSION: '5.8.0-RC2-15-gc5383ba989',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
