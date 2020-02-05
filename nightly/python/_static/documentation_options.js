936d2432b02df01340f781ba998022c95f9950b3
buildbot <buildbot@kwrobot02>
2020-02-05 15:46:09 -0500

    update docs for '5.8.0-RC2-196-g66d8cc46ed' as 'nightly'

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.8.0-RC2-190-gbc8e7a27a9',
+    VERSION: '5.8.0-RC2-196-g66d8cc46ed',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
