b99064ab8700845f43e15268450af15ad2d1a7d4
Vicente Adolfo Bolea Sanchez <vicente.bolea@kitware.com>
2021-01-27 17:58:54 +0100

    Update documentation for version nightly

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.9.0-362-g446518d53e',
+    VERSION: '5.9.1-1120-gc561f26ad5',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
