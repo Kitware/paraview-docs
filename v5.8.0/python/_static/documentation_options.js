d8f1249db8bdcc7aeb33f1b89fd2862f8dbc4fe2
buildbot <buildbot@kwrobot02>
2024-06-23 07:35:09 +0000

    Update documentation for version nightly

---
+++ v5.8.0/python/_static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '5.8.0-RC3-1-g56631fdd9a',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
