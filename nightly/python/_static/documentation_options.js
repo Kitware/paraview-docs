e8c2424edd27c4f09b6a0d307a08c3a927e48d43
buildbot <buildbot@kwrobot02>
2024-06-27 08:15:45 +0000

    Update documentation for version nightly

--- nightly/python/_static/documentation_options.js
+++ nightly/python/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '5.12.1-1222-g523b3d4161',
+    VERSION: '5.12.1-1261-ga73004c7df',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
